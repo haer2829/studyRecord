30b189b474bc14c9adc44b8a16a52aa1e0b13a30
haer <280142829@qq.com>
2023-12-25 22:16:23 +0800

    Initial commit

---
+++ src/test/java/com/haer/studyrecord/StudyRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haer.studyrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
